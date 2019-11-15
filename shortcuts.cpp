25a5a80b84b745f78bdc050a742fa092fd35b2b8
sarah <sarahannekushner@yahoo.com>
2019-11-15 18:19:29 -0500

    adding so i can delete locally

--- shortcuts.cpp
+++ shortcuts.cpp
@@ -1,8 +1,15 @@
+#include "DGtal/base/Common.h"
+
 #include "DGtal/helpers/Stddefs.h"
 #include "DGtal/helpers/Shortcuts.h"
 #include "DGtal/helpers/ShortcutsGeometry.h"
+#include "DGtal/io/Display3D.h"
 #include "DGtal/io/boards/Board3D.h"
+#include "DGtal/shapes/Mesh.h"
+#include "DGtal/io/readers/MeshReader.h"
 #include "DGtal/images/imagesSetsUtils/SetFromImage.h"
+#include "DGtal/shapes/MeshVoxelizer.h"
+#include "DGtal/topology/DigitalSetBoundary.h"
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
@@ -10,6 +17,9 @@
 
 #include <igl/writeDMAT.h>
 #include <igl/readDMAT.h>
+#include <igl/writeOBJ.h>
+
+#include <igl/readSTL.h>
 
 #include <Eigen/Core>
 
@@ -34,8 +44,10 @@ typedef DigitalTopology< Adj6, Adj18 > DT6_18;
 Adj6 adj6;
 Adj18 adj18;
 DT6_18 dt6_18( adj6, adj18, JORDAN_DT );
-typedef DigitalTopology< Adj6, Adj18 > DT6_18;
+//typedef DigitalTopology< Adj6, Adj18 > DT6_18;
 typedef Object<DT6_18, DigitalSet> ObjectType;
+
+typedef DigitalSetBoundary< Z3i::KSpace, DigitalSet > Boundary;
 
 namespace po = boost::program_options;
 
@@ -47,7 +59,7 @@ int main( int argc, char** argv )
   ("help,h", "display this message")
   ("inputFilename,f", po::value<std::string>(), "input matrix of visibilities (.dmat)" )
   ("outputFilename,o", po::value<std::string>(), "output obj file (.obj)" )
-  ("isovalue,i",  po::value<int>()->default_value(1), "isovalue (not normalized) of surface to extract in [1, num_views]" );
+  ("isovalue,i",  po::value<float>()->default_value(1.0), "isovalue (not normalized) of surface to extract in [1, num_views]" );
 
   bool parseOK=true;
   po::variables_map vm;
@@ -87,7 +99,7 @@ int main( int argc, char** argv )
 
   inputFilename = vm["inputFilename"].as<std::string>();
   outputFilename = vm["outputFilename"].as<string>();
-  int isovalue = vm["isovalue"].as<int>();
+  float isovalue = vm["isovalue"].as<float>();
 
   auto params = SH3::defaultParameters()
             | SHG3::defaultParameters();
@@ -98,18 +110,22 @@ int main( int argc, char** argv )
     // trace.info() << "#surfels=" << surface->size() << std::endl;
 
     // auto canonical_scene = SH3::makeBinaryImage( "../hidden-supports/gpu/build/output_vol_001.pgm3d", params );
-  Eigen::MatrixXi S;
+  Eigen::MatrixXf S;
   igl::readDMAT(inputFilename, S);
   // int isovalue = 61;
-  int num_views = 500;
-  Eigen::MatrixXi S_iso = (S.array() >= isovalue).select(isovalue, S.array()-S.array());
+  Eigen::MatrixXf S_iso = (S.array() >= isovalue).select(isovalue, S.array()-S.array());
   std::cout << S_iso.rows() << " " << S_iso.cols() << std::endl;
 
   Point p1( 1, 1, 1 );
-  Point p2( 100,90,29 );
+  Point p2( 200,200,145 );
   Domain domain( p1, p2 );
+
   // canonical scene
   DigitalSet scene_set( domain );
+
+  // MeshVoxelizer<DigitalSet, 6> voxelizer;
+  // Mesh<Point> a3DMesh;
+  // bool importOK = a3DMesh << "../../gp-cli/build/birds.off";
 
   trace.beginBlock( "Making digital set from the isovalue matrix. " ); 
 
@@ -127,7 +143,7 @@ int main( int argc, char** argv )
   trace.beginBlock( "Getting the border of the digital set. " ); 
   ObjectType sc( dt6_18, scene_set );
 
-  ObjectType b_scene = sc.border(); // one component
+  ObjectType b_scene = sc.border();
 
   std::vector< ObjectType > objects;
   std::back_insert_iterator< std::vector< ObjectType > > inserter( objects );
@@ -139,23 +155,34 @@ int main( int argc, char** argv )
   board << SetMode3D(domain.className(), "Paving");
   board << CustomColors3D(Color(250, 0,0),Color(250, 0,0));
 
-  for(auto it : b_scene.pointSet())
-    board << it;
+  // typedef KhalimskySpaceND<3,int> KSpace;
+  // KSpace ks;
+  board << b_scene;
+  // DigitalSet border = b_scene.pointSet();
+  // std::cout << "border made" << std::endl;
+  // Boundary boundary(ks, border);
+  // std::cout << boundary.nbSurfels() << std::endl;
 
-  board.saveOBJ(outputFilename);
+  // std::cout << typeid(boundary).name() << std::endl;
 
 
-  // trace.beginBlock( "Making a binary image from the iso value matrix. " ); 
-  // CountedPtr<SH3::BinaryImage> b_image = CountedPtr<SH3::BinaryImage>(new SH3::BinaryImage( domain ));
+  // for( SurfelConstIterator it = boundary.begin(); it != boundary.end(); ++it )
+  //   std::cout << typeid(it).name() << std::endl;
 
-  // int j = 0;
-  // for(SH3::BinaryImage::Domain::ConstIterator it = b_image->domain().begin();
-  //                     it != b_image->domain().end(); it++)
-  // {
-  //   b_image->setValue(*it, S_iso.row(j)(0));
-  //   j++;
-  // }
-  // trace.endBlock();
+  board.saveOBJ("border.obj");
+
+
+  trace.beginBlock( "Making a binary image from the iso value matrix. " ); 
+  CountedPtr<SH3::BinaryImage> b_image = CountedPtr<SH3::BinaryImage>(new SH3::BinaryImage( domain ));
+
+  int j = 0;
+  for(SH3::BinaryImage::Domain::ConstIterator it = b_image->domain().begin();
+                      it != b_image->domain().end(); it++)
+  {
+    b_image->setValue(*it, S_iso.row(j)(0));
+    j++;
+  }
+  trace.endBlock();
 
   // trace.beginBlock( "Making a grayscale image from the visibilities matrix. " );   
   // CountedPtr<SH3::GrayScaleImage> g_image = CountedPtr<SH3::GrayScaleImage>(new SH3::GrayScaleImage( domain ));
@@ -169,19 +196,58 @@ int main( int argc, char** argv )
   // }
   // trace.endBlock();
 
-  // trace.beginBlock( "Making digital surface from binary image. " );
-  // SH3::Surfel2Index s2i;
-  // auto K         = SH3::getKSpace( b_image );
-  // auto surface   = SH3::makeDigitalSurface( b_image, K, params );
-  // trace.info() << "#surfels=" << surface->size() << std::endl;
+  trace.beginBlock( "Making digital surface from binary image. " );
+  auto K         = SH3::getKSpace( b_image );
+  auto surface   = SH3::makeDigitalSurface( b_image, K, params );
+  trace.info() << "#surfels=" << surface->size() << std::endl;
+  trace.endBlock();
 
-  // //auto trisurf = SH3::makeTriangulatedSurface( s2i, surface );
-  // //auto mesh   = SH3::makeMesh( trisurf );
-  // //trace.info() << "#mesh=" << mesh->nbVertex() << std::endl;
+  trace.beginBlock( "Making polygonal surface. " );
+  SH3::Cell2Index c2i;
+  SH3::Surfel2Index s2i;
+
+  // auto polysurf = SH3::makePrimalPolygonalSurface( c2i, surface );
+  //auto mesh   = SH3::makeMesh( trisurf );
+  //trace.info() << "#mesh=" << mesh->nbVertex() << std::endl;
+  auto surfels   = SH3::getSurfelRange( surface, params );
+  auto pointels  = SH3::getPointelRange( c2i, surface );
+
+  Eigen::MatrixXf V(pointels.size(),3);
+  Eigen::MatrixXi F(surfels.size(),4);
+
+  std::cout << "V size " << V.rows() << " " << V.cols() << std::endl;
+  std::cout << "F size " << F.rows() << " " << F.cols() << std::endl;
+
+  const SH3::KSpace& space = SH3::refKSpace( surface );
+  auto embedder = SH3::getCellEmbedder( surface );
   
-  // auto ok      = SH3::saveOBJ( surface, SH3::RealVectors(), SH3::Colors(),
-  //                                "test.obj");
-  // trace.endBlock();
+  int num_p = 0;
+  for(auto&& pointel : pointels)
+  {
+    SH3::RealPoint p = embedder( pointel );
+    V.row(num_p) = Eigen::RowVector3f(p[0],p[1],p[2]);
+    num_p++;
+  }
+  int num_f = 0;
+  for(auto s : surfels)
+  {
+    Eigen::RowVector4i face;
+    auto primal_vtcs = SH3::getPointelRange( space, s );
+    int f_i = 0;
+    for ( auto&& primal_vtx : primal_vtcs )
+    {
+      face(f_i) = c2i[ primal_vtx ];
+      f_i ++;
+    }
+    F.row(num_f) = face;
+    num_f++;
+  }
+
+  igl::writeOBJ("igl-test.obj",V,F);
+  auto ok = SH3::saveOBJ( surface, SH3::RealVectors(), SH3::Colors(),
+                                 "test.obj");
+  
+  trace.endBlock();
 
   // trace.beginBlock( "Wrapping a digital set around image. " );
   // typedef functors::IntervalForegroundPredicate<SH3::GrayScaleImage> ThresholdedImage;
